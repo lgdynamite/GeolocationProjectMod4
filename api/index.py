ad84b34604bd9f761405a776c87a234561478b87
lgdynamite <ospoffical@gmail.com>
2023-10-02 17:17:46 +0000

    changed index.py file to make it applicable to the vercel website

--- api/index.py
+++ api/index.py
@@ -1,5 +1,15 @@
 import urllib.parse
 import requests
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
 
 main_api = "http://ip-api.com/json/"
 
