82f8d5cf560f08857ef493b394e9ec617599f707
lgdynamite <ospoffical@gmail.com>
2023-10-02 17:24:12 +0000

    Added new line for ISP information and changed import requests to hopefully fix the http server

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
-import urllib.parse
-import requests
+from http.server import urllib.parse
+from http.server import requests
 from http.server import BaseHTTPRequestHandler
+
+main_api = "http://ip-api.com/json/"
+
+json_data = requests.get(main_api).json()
  
 class handler(BaseHTTPRequestHandler):
  
@@ -8,17 +12,14 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        if json_data["status"] == "success":
+            self.wfile.write("IP: " + (json_data["query"]).encode('utf-8'))
+            self.wfile.write("Your Internet service provider is: " + (json_data["isp"]).encode('utf-8'))
+            self.wfile.write("ISP AS#: " + (json_data["as"]).encode('utf-8'))
+            self.wfile.write("Your location is: " + (json_data["country"]) + ", " + (json_data["region"]) + ", " + (json_data["city"]) + ", " + (json_data["zip"]).encode('utf-8'))
+            self.wfile.write("Your cordinates are: " + str(json_data["lat"]) + ", " + str(json_data["lon"]).encode('utf-8'))
+        else:
+            self.wfile.write("Check your ethernet connection.")
         return
 
-main_api = "http://ip-api.com/json/"
 
-json_data = requests.get(main_api).json()
-
-if json_data["status"] == "success":
-    print("IP: " + (json_data["query"]))
-    print("Your Internet service provider is: " + (json_data["isp"]) + " | Their AS#: " + (json_data["as"]))
-    print("Your location is: " + (json_data["country"]) + ", " + (json_data["region"]) + ", " + (json_data["city"]) + ", " + (json_data["zip"]))
-    print("Your cordinates are: " + str(json_data["lat"]) + ", " + str(json_data["lon"]))
-else:
-    print("Check your ethernet connection.")
