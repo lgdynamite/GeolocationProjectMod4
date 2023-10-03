66cd68d6d594c5e79ef42fc97bc4766bf8f5f94d
lgdynamite <ospoffical@gmail.com>
2023-10-03 01:58:33 +0000

    Fixed a few )

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write("IP: " + (json_data["query"]))
             self.wfile.write("Your Internet service provider is: " + (json_data["isp"]))
             self.wfile.write("ISP AS#: " + (json_data["as"]))
-            self.wfile.write("Your location is: " + (json_data["country"]) + ", " + (json_data["region"]) + ", " + (json_data["city"]) + ", " + (json_data["zip"])))
+            self.wfile.write("Your location is: " + (json_data["country"]) + ", " + (json_data["region"]) + ", " + (json_data["city"]) + ", " + (json_data["zip"]))
             self.wfile.write("Your cordinates are: " + str(json_data["lat"]) + ", " + str(json_data["lon"]))
         else:
             self.wfile.write("Check your ethernet connection.")
